2582504a44cb78d1910e9d611da07ab8c8e459eb
Lynda Trad <lynda.trad@etu.u-pec.fr>
2019-02-12 08:54:54 -0800

    Changement du Makefile, j'ai mis le main dans le shell.c et supprimé shell.h

--- Makefile
+++ Makefile
@@ -1,22 +1,24 @@
 EXEC=shell.exe
-SOURCES=shell.c main.c
+SOURCES=shell.c
 OBJECTS=$(SOURCES:.c=.o)
 CC=gcc
-CFLAGS=-W -Wall -std=gnu99 -g
+CFLAGS=-W -Wall -Werror -std=gnu99 -g
 LFLAGS=-lm
  
 .PHONY: default clean
  
 default: $(EXEC)
  
-shell.o: shell.c shell.h
-main.o: main.c shell.c shell.h
  
 %.o: %.c
 	$(CC) -o $@ -c $< $(CFLAGS)
  
 $(EXEC): $(OBJECTS)
 	$(CC) -o $@ $^ $(LFLAGS)
+ 
+ 
+shell :
+	$(CC) $(CFLAGS) shell.c -o shell.exe
  
 clean:
 	rm -rf $(EXEC) $(OBJECTS) $(SOURCES:.c=.c~) $(SOURCES:.c=.h~) Makefile~
--- shell.c
+++ shell.c
@@ -17,42 +17,113 @@ void affiche_cmd(char *argv[])
 	printf("\n");
 }
 	
-int parse_line(char *s, char * *argv[])
+int parse_line(char *s, char * *argv[]) //char* argv[]  plutot
 {
 // decoupe la chaine s terminée par \0, mots séparés dun espace dans case dif du tableau, 
 // case NULL à la fin du tableau
-	
-	unsigned int j;
-	j = 0;
-	
-	char *tmp = malloc(sizeof(char));
-	unsigned int tmp_int;
-	tmp_int = 0;
-	
-	while(s[i])
-	{
-		while(s[i] == ' ')
-			++i; //nouvelle case
-		
-		//cpy dans case j
-	
-		while(s[i] != ' ') // use of strpbrk ?
-		{
 
-			tmp[tmp_int] = s[i];
-			++tmp_int;
+	unsigned int i;
+	unsigned int s_int;
+	unsigned int argv_int;
+	
+	char* str;
+	
+	s_int = 0;
+	argv_int = 0;
+	
+	str = malloc(sizeof(char) + 1);
+	argv[argv_int] = malloc(sizeof(char) + 1);
+	
+	while ( s[s_int] != '\0' ){
+		i = 0;
+		while ( str[i] = s[s_int] != ' ' && s[s_int] != '\0'){
+			str = realloc(str, i + 2);
 			++i;
+			++s_int;
 		}
+		str[i] = '\0';
 		
-		++j;
-		++i;
+		argv[argv_int] = malloc(strlen(str[i]) + sizeof(char));
+		strcpy(str, argv[argv_int]);
+		
+		++argv_int;
 	}
 	
-	argv[0] = NULL; 
+	++argv_int;
+	argv[argv_int] = NULL;
 	
 	return 0;
 	
 }
+
+
+
+
+int main(int argc, char **argv){
+	printf("\n------ Affiche_cmd\n");
+	
+	affiche_cmd(argv);
+// 	char * tab[100];//static
+	char **tab = malloc( 100 * sizeof(char*));
+	
+	char *e = "commande -v toto  tata";
+	char *s = malloc(1024 * sizeof(char));
+	
+	strcpy (s,e);
+// 	parse_line(s,tab);
+	
+	
+// 	parse_line(s,&tab);
+
+	// on met \0 a la fin des mots on modifie la chaine s et on met dans tab les pointeurs vers chaque debut de mot
+	// eviter alloc dynamique dans parse line
+	exit(EXIT_SUCCESS);
+	
+}
+
+
+
+
+
+//on met \0 a la fin des mots on modifie la chaine s et on met dans tab les pointeurs vers chaque debut de mot 
+
+// int parse_line(char *s, char * *argv[])
+// {
+// // decoupe la chaine s terminée par \0, mots séparés dun espace dans case dif du tableau, 
+// // case NULL à la fin du tableau
+// 
+// 	unsigned int i;
+// 	unsigned int s_int;
+// 	unsigned int argv_int;
+// 	
+// 	char* str;
+// 	
+// 	s_int = 0;
+// 	argv_int = 0;
+// 	
+// 	str = malloc(sizeof(char) + 1);
+// 	argv[argv_int] = malloc(sizeof(char) + 1);
+// 	
+// 	while ( s[s_int] != '\0' ){
+// 		
+// 		i = 0;
+// 		
+// 		while ( str[i] = s[s_int] != ' ' && s[s_int] != '\0'){
+// 			str = realloc(str, i + 2);
+// 			++i;
+// 			++s_int;
+// 		}
+// 		str[i] = '\0';
+// 	
+// 		str[i] = argv[argv_int] ; // need malloc
+// 		++argv_int;
+// 	}
+// 	
+// 	return 0;
+// 	
+// }
+
+
 
 // char *strpbrk(const char *s, const char *accept);
 // 
