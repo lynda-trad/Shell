fee7b29785f2da776f012fa0104044fb57a36a6e
Lynda Trad <lynda.trad@etu.u-pec.fr>
2019-02-26 08:25:05 -0800

    shell.c

--- shell.c
+++ shell.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
 
 void affiche_cmd(char *argv[])
@@ -21,7 +22,7 @@ void affiche_cmd(char *argv[])
 	printf("\n");
 }
 
-int parse_line(char *s, char **argv[])
+int parse_line(char *s, char * *argv[])
 {
 	unsigned int i;
 	unsigned int len;
@@ -39,6 +40,7 @@ int parse_line(char *s, char **argv[])
 		{
 			++i;
 		}
+		
 		debw = &s[i];
 		wordl = 0;
 		
@@ -50,18 +52,16 @@ int parse_line(char *s, char **argv[])
 		
 		if(wordl)
 		{
+			tmp[len] = malloc(sizeof(char) * wordl + 1);
+			
+			memcpy(tmp[len], debw, wordl);
+			
+			tmp[len][wordl]= '\0';
+			
 			++len;
 			
 			tmp = realloc(tmp, sizeof(char*) * (len + 1));
-			
-			tmp[len-1] = malloc(sizeof(char) * wordl + 1);
-			
-			memcpy(tmp[len-1], debw, wordl);
-			
-			tmp[len-1][wordl]= '\0';
-			
 		}
-		
 	}
 	
 	tmp[len] = NULL;
@@ -70,7 +70,7 @@ int parse_line(char *s, char **argv[])
 	return len;
 }
 
-void simple_cmd(char *argv[])
+void simple_cmd(char *argv[])  // NOT OK
 {
 	unsigned int i;
 	i = 0;
@@ -81,22 +81,64 @@ void simple_cmd(char *argv[])
 		{
 			exit(EXIT_FAILURE);
 		}
-		else if(strcmp(argv[i],"cd"))
+		else 
+		if(strcmp(argv[i],"cd"))
 		{
-// 			int status;
 			chdir(argv[i+1]);
-// 			waitpid(-1,&status,0);
+			printf("%d",i); // 0134 2 = "cd"
 		}
 		++i;
 	}
+	
 }
 
-
-
-int main()//int argc, char **argv
+/*
+int parse_line_redir(char *s, char **argv[], char **in, char **out)
 {
+	//analyse chaine cm parse_line mais prend en compte redirections < en entree et > en sortie 
+	//nom de fichier en entree et sortie stockés dans in et out respectivement
+	//variable correspondante a null si une redirection n'a pas lieu
 	
-	while(1) //exit avec CTRL+C et pas CTRL+D -> how ?
+}*/
+
+/*
+int redir_cmd(char *argv[], char*in, char *out)
+{
+	//use simple_cmd NOT OK YET
+	 
+	
+}
+*/
+
+
+int main(int argc, char **argv)
+{
+	if(argc >1)
+	{
+		char *dir = malloc(sizeof(char) * 1024);
+		
+		char **tab = malloc( 100 * sizeof(char*));
+		
+		char *s = malloc(1024 * sizeof(char));
+		
+		int fd = open(argv[1],O_RDONLY);
+		if (fd < 0)
+			exit(EXIT_FAILURE);
+		
+		read(fd, s, 1024);
+		
+		parse_line(s,&tab);
+		
+// 		simple_cmd(tab);
+		
+		printf("%s$",getcwd(dir,1024));
+		affiche_cmd(tab);
+		
+		free(dir);
+		free(tab);
+		free(s);
+	}
+	else while(1) //exit avec CTRL+C et pas CTRL+D -> how ?
 	{
 		char *dir = malloc(sizeof(char) * 1024);
 		
@@ -105,11 +147,13 @@ int main()//int argc, char **argv
 		char *s = malloc(1024 * sizeof(char));
 		
 		printf("%s$",getcwd(dir,1024));
+		
 		fgets(s,1024,stdin);
 		
 		parse_line(s,&tab);
 		
 // 		simple_cmd(tab);
+		
 		affiche_cmd(tab);
 		
 		free(dir);
