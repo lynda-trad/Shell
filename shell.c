c3e52803189d86e61fbe682e9129f49eb014ccdc
Lynda Trad <lynda.trad@etu.u-pec.fr>
2019-03-30 15:15:22 -0700

    shell.c

--- shell.c
+++ shell.c
@@ -67,7 +67,7 @@ int parse_line(char *s, char * *argv[])
 	return len;
 }
 
-void simple_cmd(char *argv[])  // NOT OK
+void simple_cmd(char *argv[])
 {
 // 	printf("%s",argv[0]); //il y a un \n a la fin du argv[0] 
 	if(!*argv)
@@ -85,10 +85,13 @@ void simple_cmd(char *argv[])  // NOT OK
 	else
 	{
 		pid_t p = fork();
+		int status;
 		if(p == 0)
 		{
 			execvp(argv[0],argv);
 		}
+		else
+			wait(&status);
 	}
 }
 
@@ -203,7 +206,6 @@ int parse_line_redir(char *s, char **argv[], char **in, char **out)
 	return i;
 }
 
-
 int redir_cmd(char *argv[], char*in, char *out)
 {
 	simple_cmd(argv); 
@@ -262,104 +264,6 @@ int parse_line_pipes(char *s, char **argv[], char **in, char **out)
 	return i;
 }
 
-
-/*
-	{
-	{
-		if(s[i] == '<') //in
-		{
-			//gives input to a command
-			out[0] = NULL;
-			int j;
-			j = i+1;
-			
-			while (s[j] == ' ')
-			{
-				++j;
-			}
-			
-			debw = &s[j];
-			wordl = 0;
-			in[0] = malloc(sizeof(char*) * 1);
-			
-			while(s[j] && s[j] != ' ')
-			{
-				++wordl;
-				++j;
-			}
-			
-			if(wordl)
-			{
-				in[0] = malloc(sizeof(char) * wordl + 1);
-				
-				memcpy(in[0], debw, wordl);
-				
-				in[0][wordl]= '\0';
-			}
-		}
-		else if(s[i] == '>') //out
-		{
-			//directs the output of a command into a file
-			in[0] = NULL;
-			int j;
-			j = i+1;
-			
-			while (s[j] == ' ')
-			{
-				++j;
-			}
-			
-			debw = &s[j];
-			wordl = 0;
-			out[0] = malloc(sizeof(char*) * 1);
-			
-			while(s[j] && s[j] != ' ')
-			{
-				++wordl;
-				++j;
-			}
-			
-			if(wordl)
-			{
-				out[0] = malloc(sizeof(char) * wordl + 1);
-				
-				memcpy(out[0], debw, wordl);
-				
-				out[0][wordl]= '\0';
-			}
-		}
-		
-		debw = &s[i];
-		wordl = 0;
-		
-		while(s[i] && s[i] != ' ')
-		{
-			++wordl;
-			++i;
-		}
-		
-		if(wordl)
-		{
-			tmp[len] = malloc(sizeof(char) * wordl + 1);
-			
-			memcpy(tmp[len], debw, wordl);
-			
-			tmp[len][wordl]= '\0';
-			
-			++len;
-			
-			tmp = realloc(tmp, sizeof(char*) * (len + 1));
-		}
-		
-	}
-	
-	tmp[len] = NULL;
-	argv[0] = tmp;
-	}
-	}
-*/
-
-
 //pour pipe
 int redir_cmd_pipe(char *argv[], char*in, char *out)
 {
@@ -380,10 +284,9 @@ int redir_cmd_pipe(char *argv[], char*in, char *out)
 	return fd;
 }
 
-
 int main(int argc, char **argv)
 {
-	if(argc > 1) //nom de fichier en argument
+	if(argc > 1) // nom de fichier en argument
 	{
 		char *dir = malloc(sizeof(char) * 1024);
 		
@@ -396,7 +299,7 @@ int main(int argc, char **argv)
 			exit(EXIT_FAILURE);
 		
 		printf("%s$ ",getcwd(dir,1024));
-		affiche_cmd(tab);
+// 		affiche_cmd(tab);
 		
 		read(fd, s, 1024);
 		
@@ -428,7 +331,7 @@ int main(int argc, char **argv)
 		}
 		
 		parse_line(s,&tab);
-		affiche_cmd(tab);
+// 		affiche_cmd(tab);
 		
 		simple_cmd(tab);
 		
